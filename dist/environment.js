eceaa69e080c72fb1ef2e87bd8ff965b89ac1c5b
unknown <sasikumar.9799@gmail.com>
2023-04-22 19:26:29 +0530

    unusedfileDeleted

--- dist/environment.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.environment = void 0;
-exports.environment = {
-    mongoDBUrl: 'mongodb+srv://sasi358459:Sasi358459@cluster0.h2vvopf.mongodb.net/?',
-    PORT: 3000
-};
-//# sourceMappingURL=environment.js.map
